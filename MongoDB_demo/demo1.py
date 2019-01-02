c0b74d7cef3da3c79eca70495948b42c8c389a2b
Mocha-Pudding <zhujiangge6@126.com>
2019-01-02 14:46:55 +0800

    Python操作MongoDB数据库
    MongoDB数据库基本操作(完结)

--- MongoDB_demo/demo1.py
+++ MongoDB_demo/demo1.py
@@ -15,31 +15,42 @@ collection = db.qa
 # "增"、“删”、“改”、“查”具体操作示例：
 
 # 1.“增”：插入数据
-# collection.insert({"username":"ZGreenicy","age":"24"})
+collection.insert({"username":"ZGreenicy","age":"24"})
 # 1.1 insert_one插入一条数据，等价于insert 如上
-# collection.insert_one({
-#     "username":"JS6",
-#     "age":"2"
-# })
+collection.insert_one({
+    "username":"JS6",
+    "age":"2"
+})
 # 1.2 insert_many插入多条数据到集合中，如下
-# collection.insert_many([
-#     {
-#         "username":"Zhujiange",
-#         "age":28
-#     },
-#     {
-#         "username":"Zengjingxin",
-#         "age":"24"
-#     }
-# ])
+collection.insert_many([
+    {
+        "username":"Zhujiange",
+        "age":28
+    },
+    {
+        "username":"Zengjingxin",
+        "age":"24"
+    }
+])
 
 # 2.“查”：查找数据
 # 2.1 fing方法，获取集合中所有的数据
-# cursor = collection.find()
-# for x in cursor:
-#     print(x)
+cursor = collection.find()
+for x in cursor:
+    print(x)
 # 2.2 fing_one方法：获取集合中一条文档对象
-# result = collection.find_one({"age":28})
-# print(result)
+result = collection.find_one({"age":28})
+print(result)
 
+# 3.“改”：更新修改数据
+# 3.1 更新一条文档对象
+collection.update_one({"username":"aaa"},{"Set":{"username":"ccc"}})
+# 3.2 更新多条文档对象
+collection.update_many({"username":"aaa"},{"Set":{"username":"bbb"}})
 
+# 4.“删”：删除数据
+# 4.1 删除一条文档对象
+collection.delete_one({"username":"ccc"})
+
+# 4.2 删除多条文档对象
+collection.delete_many({"username":"ddd"})
