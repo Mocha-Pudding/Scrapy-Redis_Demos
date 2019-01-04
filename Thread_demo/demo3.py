85727779b9edc17ad663b8eb8f4ae1ee0d0dbbb8
Mocha-Pudding <zhujiangge6@126.com>
2019-01-04 14:56:51 +0800

    多线程爬虫
    1.Thread类创建多线程；
    2.多线程共享全局变量；
    3.锁机制

--- Thread_demo/demo3.py
+++ Thread_demo/demo3.py
@@ -1,13 +1,17 @@
-# 多线程共享全局变量的问题
+# 多线程共享全局变量的问题 以及锁机制
 
 import threading
 
 VALUE = 0
 
+gLock = threading.Lock()   #创建锁
+
 def add_value():
-    global VALUE
-    for x in range(1000):
+    global VALUE      #使用全局变量，需要用global关键字来声明
+    gLock.acquire()   #上锁
+    for x in range(1000000):
         VALUE += 1
+    gLock.release()   #释放锁
     print('value: %d'  %VALUE)
 
 def main():
