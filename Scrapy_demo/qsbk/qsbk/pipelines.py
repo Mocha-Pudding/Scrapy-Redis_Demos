5e65ac86be10712206bc859520b6c272fa170035
Mocha-Pudding <zhujiangge6@126.com>
2019-02-27 23:03:23 +0800

    Scrapy_demo
    糗事百科段子 qsbk_spider

--- Scrapy_demo/qsbk/qsbk/pipelines.py
+++ Scrapy_demo/qsbk/qsbk/pipelines.py
@@ -6,16 +6,63 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 
+########## 方法一 ##########
+# class QsbkPipeline(object):
+#     def __init__(self):
+#         self.fp = open("duanzi.json", "w", encoding='utf-8')
+#
+#     def open_spider(self, spider):
+#         print('爬虫开始了……')
+#
+#     def process_item(self, item, spider):
+#         item_json = json.dumps(dict(item), ensure_ascii=False)
+#         self.fp.write(item_json+'\n')
+#         return item
+#
+#     def close_spider(self, spider):
+#         self.fp.close()
+#         print('爬虫结束了…')
+
+
+########## 方法二 ##########
+#### 数据量比较少的时候 可以用JsonItemExporter
+from scrapy.exporters import JsonItemExporter
+
+# class QsbkPipeline(object):
+#     def __init__(self):
+#         self.fp = open("duanzi.json", "wb")   #以二进制的方式写入
+#         self.exportr = JsonItemExporter(self.fp, ensure_ascii=False, encoding='utf-8')
+#         self.exportr.start_exporting()
+#
+#     def open_spider(self, spider):
+#         print('爬虫开始了……')
+#
+#     def process_item(self, item, spider):
+#         # item_json = json.dumps(dict(item), ensure_ascii=False)
+#         # self.fp.write(item_json+'\n')
+#         self.exportr.export_item(item)
+#         return item
+#
+#     def close_spider(self, spider):
+#         self.exportr.finish_exporting()
+#         self.fp.close()
+#         print('爬虫结束了…')
+
+
+########## 方法三 ##########
+#### 数据量比较多的时候，需要用JsonLinesItemExporter
+from scrapy.exporters import JsonLinesItemExporter
+
 class QsbkPipeline(object):
     def __init__(self):
-        self.fp = open("duanzi.json", "w", encoding='utf-8')
+        self.fp = open("duanzi.json", "wb")   #以二进制的方式写入
+        self.exportr = JsonLinesItemExporter(self.fp, ensure_ascii=False, encoding='utf-8')
 
     def open_spider(self, spider):
         print('爬虫开始了……')
 
     def process_item(self, item, spider):
-        item_json = json.dumps(dict(item), ensure_ascii=False)
-        self.fp.write(item_json+'\n')
+        self.exportr.export_item(item)
         return item
 
     def close_spider(self, spider):
--- Scrapy_demo/qsbk/qsbk/spiders/qsbk_spider.py
+++ Scrapy_demo/qsbk/qsbk/spiders/qsbk_spider.py
@@ -11,6 +11,7 @@ class QsbkSpiderSpider(scrapy.Spider):
     allowed_domains = ['qiushibaike.com']
     #start_urls：爬虫从这个变量中的url开始
     start_urls = ['https://www.qiushibaike.com/text/page/1/']
+    base_domain = "https://www.qiushibaike.com"
 
     def parse(self, response):
         contentLeft = response.xpath("//div[@id='content-left']")
@@ -25,3 +26,8 @@ class QsbkSpiderSpider(scrapy.Spider):
             item = QsbkItem(author=author, content=content)
             # duanzi = {"author":author, "content":content}    #定义一个字典存放
             yield item
+        next_url = response.xpath("//ul[@class='pagination']/li[last()]/a/@href").get()
+        if not next_url:
+            return
+        else:
+            yield scrapy.Request(self.base_domain+next_url,callback=self.parse) 
