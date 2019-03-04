6e26f958a4511a357d4c0954a26a956079fb4d06
Mocha-Pudding <zhujiangge6@126.com>
2019-03-05 00:19:40 +0800

    douban_login

--- Scrapy_demo/douban_login/douban_login/settings.py
+++ Scrapy_demo/douban_login/douban_login/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'douban_login.spiders'
 #USER_AGENT = 'douban_login (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -39,10 +39,11 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en',
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
--- Scrapy_demo/douban_login/douban_login/spiders/douban.py
+++ Scrapy_demo/douban_login/douban_login/spiders/douban.py
@@ -1,11 +1,42 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from urllib import request
+from PIL import Image
 
 class DoubanSpider(scrapy.Spider):
     name = 'douban'
     allowed_domains = ['douban.com']
-    start_urls = ['http://douban.com/']
+    start_urls = ['https://accounts.douban.com/passport/login']
+    login_url = 'https://accounts.douban.com/passport/login'
 
     def parse(self, response):
-        pass
+        formdata = {
+            'source': 'None',
+            'redir':'https://www.douban.com/',
+            'form_email':'253496831@qq,com',
+            'form_password':'pythonspider',
+            'remember':'on',
+            'login':'登录'
+        }
+        captcha_url = response.css('img#captcha_image:attr(src)').get()
+        # print('='*60)
+        # print(captcha_img)
+        if captcha_url:
+            captcha = self.recognize_captcha(captcha_url)
+            formdata['captcha-solution'] = captcha
+            captcha_id = response.xpath("//input[@name='captcha-id']/@value").get()
+            formdata['captcha-id'] = captcha_id
+        yield scrapy.FormRequest(url=self.login_url,formdata=formdata,callback=self.parse_after_login)
+
+    def parse_after_login(self, response):
+        if response.url == 'https://www.douban.com/':
+            print('登录成功！')
+        else:
+            print('登录失败！')
+
+    def recognize_captcha(self,image_url):
+        request.urlretrieve(image_url, 'captcha.png')
+        image = Image.open('captcha.png')
+        image.show()
+        captcha = input('请输入验证码：')
+        return captcha
